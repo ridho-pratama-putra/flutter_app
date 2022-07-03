199e66f69c260b21cc518045845b1b567be4d8c1
ridho <ridho.putra@btpn.com>
2022-07-03 20:57:05 +0700

    try to implement model and constants

---
+++ lib/pair_question_and_answer_model.dart
@@ -0,0 +1,8 @@
+class PairQuestionAndAnswer {
+  String question;
+  String fact;
+  bool? keyAnswer;
+
+  PairQuestionAndAnswer(
+      {required this.question, required this.fact, this.keyAnswer});
+}
